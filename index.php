45099c0dc54376f15db6a5ac68a4b62eabe914d3
Ali <ali@brainlet.co>
2020-03-27 22:20:18 +0500

    Use fizzbuzz algorithm to print numbers.

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
+include_once(__DIR__ . "/vendor/autoload.php");
 
-include_once(__DIR__."vendor/autoload.php");
+use App\FizzBuzz;
+
+$fizzBuzz = new FizzBuzz();
+
+for ($i = 1; $i <= 20; $i++) {
+    echo $fizzBuzz->print($i) . '<br/>';
+}
