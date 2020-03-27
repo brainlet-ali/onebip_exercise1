94d3ccf912387ea0b53d64c59986228af76acb4a
Ali <ali@brainlet.co>
2020-03-27 17:12:15 +0500

    Task # 2(Unit Test): Check if it returns the word 'Bizz' : [RED]
    If it's divisible by 5.

--- tests/FizzBuzzTest.php
+++ tests/FizzBuzzTest.php
@@ -48,5 +48,44 @@ class FizzBuzzTest extends TestCase
         ];
     }
 
+    /**
+     * @test
+     * @dataProvider buzz
+     *
+     * @param $number
+     * @param $expected
+     */
+    public function it_returns_the_word_buzz_if_divisible_by_5(
+      $number,
+      $expected
+    ) {
+        $buzz = new FizzBuzz();
+        $this->assertEquals($expected, $buzz->isBuzz($number));
+    }
 
+    public function buzz()
+    {
+        return [
+          [1, false],
+          [2, false],
+          [3, false],
+          [4, false],
+          [5, 'Buzz'],
+          [6, 6],
+          [7, false],
+          [8, false],
+          [9, false],
+          [10, false],
+          [11, false],
+          [12, false],
+          [13, false],
+          [14, false],
+          [15, false],
+          [16, false],
+          [17, false],
+          [18, false],
+          [19, false],
+          [20, 'Buzz'],
+        ];
+    }
 }
