a3b58a0bb3c6c98fd0f183fc78c87f15548a0a07
Ali <ali@brainlet.co>
2020-03-27 17:17:23 +0500

    Task # 2(Unit Test): Check if it returns the word 'Buzz' : [GREEN]
    1)Add implementation in isBuzz method.
    2)Change data provider for correct values to make the test pass.

--- src/FizzBuzz.php
+++ src/FizzBuzz.php
@@ -11,4 +11,9 @@ class FizzBuzz
     {
         return ($number % 3 === 0) ? 'Fizz' : false;
     }
+
+    public function isBuzz($number)
+    {
+        return ($number % 5 === 0) ? 'Buzz' : false;
+    }
 }
